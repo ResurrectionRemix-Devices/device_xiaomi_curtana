MoKee/android_device_xiaomi_sm6150-common

sm6150-common: Add support for event-based tap to wake gesture control
Change-Id: Id963e3c7302a7ee46a5a6b6d1b743a5718d71f4c

power-libperfmgr/Power.cpp
@@ -32,6 +32,11 @@
 #include "Power.h"
 #include "power-helper.h"
 #include "display-helper.h"
+#include <linux/input.h>
+
+constexpr char kWakeupEventNode[] = "/dev/input/event2";
+constexpr int kWakeupModeOff = 4;
+constexpr int kWakeupModeOn = 5;
 
 /* RPM runs at 19.2Mhz. Divide by 19200 for msec */
 #define RPM_CLK 19200
@@ -222,8 +227,20 @@ Return<void> Power::powerHint(PowerHint_1_0 hint, int32_t data) {
     return Void();
 }
 
-Return<void> Power::setFeature(Feature /*feature*/, bool /*activate*/)  {
-    //Nothing to do
+Return<void> Power::setFeature(Feature feature, bool activate) {
+    switch (feature) {
+        case Feature::POWER_FEATURE_DOUBLE_TAP_TO_WAKE: {
+            int fd = open(kWakeupEventNode, O_RDWR);
+            struct input_event ev;
+            ev.type = EV_SYN;
+            ev.code = SYN_CONFIG;
+            ev.value = activate ? kWakeupModeOn : kWakeupModeOff;
+            write(fd, &ev, sizeof(ev));
+            close(fd);
+            } break;
+        default:
+            break;
+    }
     return Void();
 }
 

ResurrectionRemix-Devices/device_xiaomi_curtana

power-libperfmgr/Power.cpp
@@ -32,6 +32,11 @@
 #include "Power.h"
 #include "power-helper.h"
 #include "display-helper.h"
+#include <linux/input.h>
+
+constexpr char kWakeupEventNode[] = "/dev/input/event3";
+constexpr int kWakeupModeOff = 4;
+constexpr int kWakeupModeOn = 5;
 
 /* RPM runs at 19.2Mhz. Divide by 19200 for msec */
 #define RPM_CLK 19200
@@ -222,8 +227,20 @@ Return<void> Power::powerHint(PowerHint_1_0 hint, int32_t data) {
     return Void();
 }
 
-Return<void> Power::setFeature(Feature /*feature*/, bool /*activate*/)  {
-    //Nothing to do
+Return<void> Power::setFeature(Feature feature, bool activate) {
+    switch (feature) {
+        case Feature::POWER_FEATURE_DOUBLE_TAP_TO_WAKE: {
+            int fd = open(kWakeupEventNode, O_RDWR);
+            struct input_event ev;
+            ev.type = EV_SYN;
+            ev.code = SYN_CONFIG;
+            ev.value = activate ? kWakeupModeOn : kWakeupModeOff;
+            write(fd, &ev, sizeof(ev));
+            close(fd);
+            } break;
+        default:
+            break;
+    }
     return Void();
 }
 